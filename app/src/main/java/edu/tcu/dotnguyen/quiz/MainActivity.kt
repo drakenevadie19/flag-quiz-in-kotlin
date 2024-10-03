95984967599358d0ce09298da5050d03be33ac97
drakenevadie19 <drakennievadje19@gmail.com>
2024-10-03 15:10:53 -0500

    New Change: Adding Question page, and router to it (intent)

--- app/src/main/java/edu/tcu/dotnguyen/quiz/MainActivity.kt
+++ app/src/main/java/edu/tcu/dotnguyen/quiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.tcu.dotnguyen.quiz
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
@@ -23,11 +24,29 @@ class MainActivity : AppCompatActivity() {
         val nameEt = findViewById<TextInputEditText>(R.id.name_et)
         nameEt.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_GO) { // When user click actionGO (arrow forward) button
-                Toast.makeText(this, nameEt.text.toString(), Toast.LENGTH_SHORT).show()
+//                if (nameEt.text.toString() != "") {
+//                    Toast.makeText(this, nameEt.text.toString(), Toast.LENGTH_SHORT).show()
+//                } else {
+//                    Toast.makeText(this, "Please input name", Toast.LENGTH_SHORT).show()
+//                }
+                goToQuestion(nameEt)
                 true // Since lambda expression return a boolean
             } else false
         }
     }
 
+    // goToQuestion method will deal with intent and forward
+    private fun goToQuestion(nameEt: TextInputEditText) {
+        // Deal with an empty name
+        if (nameEt.text.toString() == "") {
+            Toast.makeText(this, "Please input name", Toast.LENGTH_SHORT).show()
+        } else {
+            // Move to next page
+            val intent = Intent(this, QuestionActivity::class.java)
+            intent.putExtra("fvgbhnj", nameEt.text.toString())
+            startActivity(intent)
+            finish() // Destroy previous page, since it unnecessary
+        }
+    }
 
 }
